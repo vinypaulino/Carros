0929394f5986468704eec25d02bdcb83256cfa5a
vinypaulino <vinydos3@gmail.com>
2017-11-18 11:56:07 -0300

    Termino de Web View
    Foi adicionado funcionalidades referentes ao webview

--- app/src/main/java/br/com/vinypaulino/mobile/carros/fragments/SiteLivroFragment.java
+++ app/src/main/java/br/com/vinypaulino/mobile/carros/fragments/SiteLivroFragment.java
@@ -1,17 +1,82 @@
 package br.com.vinypaulino.mobile.carros.fragments;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import br.com.vinypaulino.mobile.carros.R;
 
-public class SiteLivroFragment extends BaseFragment{
+public class SiteLivroFragment extends BaseFragment {
+    private static final String URL_SOBRE = "http://www.livroandroid.com.br/sobre.htm";
+    private WebView webView;
+    private ProgressBar progress;
+
+    protected SwipeRefreshLayout swipeLayout;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState){
+                             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_site_livro, container, false);
+        webView = (WebView) view.findViewById(R.id.webview);
+        progress = (ProgressBar) view.findViewById(R.id.progress);
+        setWebViewClient(webView);
+        //Carrega a Pagina
+        webView.loadUrl(URL_SOBRE);
+        //Swipe to Refresh
+        swipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeToRefresh);
+        swipeLayout.setOnRefreshListener(OnRefreshListener());
+        swipeLayout.setColorSchemeResources(
+                R.color.refresh_progress_1,
+                R.color.refresh_progress_2,
+                R.color.refresh_progress_3);
+
         return view;
+    }
+
+    private SwipeRefreshLayout.OnRefreshListener OnRefreshListener() {
+        return new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webView.reload();//Atualiza a Pagina
+            }
+        };
+    }
+
+    public void setWebViewClient(WebView webView) {
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView webView, String url, Bitmap favicon) {
+                //Liga o Progress
+                progress.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView webView, String url) {
+                //Desliga o progress
+                progress.setVisibility(View.INVISIBLE);
+                //Termina a animação do Swipe to Refresh
+                swipeLayout.setRefreshing(false);
+
+
+            }
+            //metodo para interceptar uma requisição web
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                Log.d("livroandroid","webview url: " + url);
+                if (url != null && url.endsWith("sobre.htm")){
+                    AboutDialog.showAbout(getFragmentManager());
+                    //Retorna true para informar que interceptamos o evento
+                    return true;
+                }
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
     }
 }
