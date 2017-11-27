deb3ae390ecdf640f670720b5be3330888e70bdc
vinypaulino <vinydos3@gmail.com>
2017-11-27 17:30:23 -0300

    Preferences
    Salvo as preferencias do usuario de acordo com a ultima tab acessada

--- app/src/main/java/br/com/vinypaulino/mobile/carros/activity/MainActivity.java
+++ app/src/main/java/br/com/vinypaulino/mobile/carros/activity/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 
 import br.com.vinypaulino.mobile.carros.R;
 import br.com.vinypaulino.mobile.carros.adapter.TabsAdapter;
+import livroandroid.lib.utils.Prefs;
 
 import static br.com.vinypaulino.mobile.carros.fragments.AboutDialog.showAbout;
 
@@ -34,9 +35,9 @@ public class MainActivity extends BaseActivity {
 
     private void setupViewPagerTabs() {
         //ViewPager
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
         viewPager.setOffscreenPageLimit(2);
-        viewPager.setAdapter( new TabsAdapter(getContext(), getSupportFragmentManager()));
+        viewPager.setAdapter(new TabsAdapter(getContext(), getSupportFragmentManager()));
         //Tabs
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         //Cria as tabs com o mesmo adapter utilizado pelo ViewPager
@@ -44,6 +45,27 @@ public class MainActivity extends BaseActivity {
         int cor = ContextCompat.getColor(getContext(), R.color.white);
         //Cor branca no texto (o fundo azul foi definido no layout)
         tabLayout.setTabTextColors(cor, cor);
+        //Lê o índice da ultima tab utilizada no aplicativo
+        int tabIdx = Prefs.getInteger(getContext(), "tabIdx");
+        viewPager.setCurrentItem(tabIdx);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                //Salva p indice da pagina/tab selecionada
+                Prefs.setInteger(getContext(), "tabIdx", viewPager.getCurrentItem());
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
     }
 
     @Override
@@ -53,9 +75,9 @@ public class MainActivity extends BaseActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_about){
+        if (id == R.id.action_about) {
             showAbout(getSupportFragmentManager());
             return true;
         }
